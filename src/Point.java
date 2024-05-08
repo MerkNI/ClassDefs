50e05bd6453938657c9d36f6c90f028b0bdb2505
50055699 <50055699@serc.ac.uk>
2024-05-08 15:17:04 +0100

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,25 @@
+public class Point {
+
+    private double x;
+    private double y;
+
+    public Point(double anX, double aY){
+
+        this.x = anX;
+        this.y = aY;
+
+    }
+
+    public double getX() {
+
+        return this.x;
+
+    }
+
+    public double getY() {
+
+        return this.y;
+
+    }
+
+}
