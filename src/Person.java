8316211ce62447148b2dc01bb072bc31d46b5508
50055699 <50055699@serc.ac.uk>
2024-05-08 15:14:29 +0100

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String aName, int anAge){
+
+        this.name = aName;
+        this.age = anAge;
+
+    }
+
+    public String getName(){
+
+        return this.name;
+
+    }
+
+    public int getAge() {
+
+        return this.age;
+
+    }
+
+}
