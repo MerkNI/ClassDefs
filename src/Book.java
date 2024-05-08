d7d7a5e8846b3ac56a3a0e38a9b50522b02c93d2
50055699 <50055699@serc.ac.uk>
2024-05-08 15:12:12 +0100

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String aTitle, String anAuthor){
+
+        this.title = aTitle;
+        this.author = anAuthor;
+
+    }
+
+    public String getTitle(){
+
+        return  this.title;
+
+    }
+
+    public String getAuthor() {
+
+        return this.author;
+
+    }
+
+}
